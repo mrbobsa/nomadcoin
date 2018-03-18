0919ff64f3b32588c0de3bd9ecb268a8248f44c5
Min Sung Kim <ds3dxf@hanmail.net>
2018-03-18 16:19:11 +0900

    #6 createHash and createNewBlock function

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,4 +1,6 @@
- class Block {
+const CryptoJS = require("crypto-js");
+
+class Block {
     constructor (index, hash, previousHash, timestamp, data){
         this.index = index;
         this.hash = hash;
@@ -6,15 +8,40 @@
         this.timestamp = timestamp;
         this.data = data;
     }
- }
- const genesisBlock = new Block(
-     0,
-     "37B50D0DCEB1FA3D764A8F2DC3C0CC657C03C37E92D91B4F9CE1E56598A3AB87",
-     null,
-     1520779464701,
-     "This is the Genesis!!" 
- );
+}
+const genesisBlock = new Block(
+    0,
+    "37B50D0DCEB1FA3D764A8F2DC3C0CC657C03C37E92D91B4F9CE1E56598A3AB87",
+    null,
+    1520779464701,
+    "This is the Genesis!!" 
+);
 
- let blockchain = [genesisBlock];
+let blockchain = [genesisBlock];
 
-console.log(blockchain);
+const getLastBlock = () => blockchain[blockchain.length -1];
+
+const getTimeStamp = () => new Date.getTime() / 1000;
+
+const createHash = (index, previousHash, timestamp, data) => 
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+const createNewBlock = data => {
+    const previousBlock = getLastBlock();
+    const newBlockIndex = previousBlock.index + 1;
+    const newTimestamp = getTimeStamp();
+    const newHash = createHash(
+        newBlockIndex,
+        previousBlock.hash,
+        newTimestamp,
+        data
+    );
+    const newBlock = new Block(
+        newBlockIndex,
+        newHash,
+        previousBlock.hash,
+        newTimestamp,
+        data
+    );
+    return newBlock;
+};
